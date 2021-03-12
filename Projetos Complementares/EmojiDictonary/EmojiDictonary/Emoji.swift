f999452e33a947256d5facbbb77a30486bd09161
Curitiba <curitiba01@MacBook-Pro-de-Curitiba.local>
2021-03-12 15:11:00 -0300

    EmojiDictonary

---
+++ Projetos Complementares/EmojiDictonary/EmojiDictonary/Emoji.swift
@@ -0,0 +1,15 @@
+//
+//  Emoji.swift
+//  EmojiDictonary
+//
+//  Created by Curitiba on 12/03/21.
+//
+
+import Foundation
+
+struct Emoji {
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+}
